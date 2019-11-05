988b1633ff886d48040c2f14c13da005f3ca676d
“Kristhian_Deoliveira” <kristhian.deoliveira@gmail.com.com>
2019-11-05 08:32:15 -0500

    reimbusment form implementation complete

--- Maven project/Project01/src/main/java/com/revature/dao/TuitionReimbursementFormDAO.java
+++ Maven project/Project01/src/main/java/com/revature/dao/TuitionReimbursementFormDAO.java
@@ -11,5 +11,5 @@ public interface TuitionReimbursementFormDAO {
 	public List<TuitionReimbursementForm>getAllForms() throws SQLException;
 	public void submitForm(String firstName, String lastName, String username, String date, String time,
 			String city, String state, String zip, String description, Double cost, String gradingFormat,
-			String typeOfEvent, String workRelatedJustification, int daysMissedFromWork);
+			String typeOfEvent, String workRelatedJustification, int daysMissedFromWork) throws SQLException;
 }
