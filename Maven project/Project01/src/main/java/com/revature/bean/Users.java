9e60d01ba7c247e092a1f0c264d59c24a3f29464
RomanIbba <romanibba95@gmail.com>
2019-11-06 11:46:48 -0500

    updated bean classes

--- Maven project/Project01/src/main/java/com/revature/bean/Users.java
+++ Maven project/Project01/src/main/java/com/revature/bean/Users.java
@@ -1,56 +1,98 @@
 package com.revature.bean;
 
 public class Users {
-	private String username;
-	private String password;
-	private String first_name;
-	private String last_name;
-	private String department;
-	
-	
-	public Users(String username, String password, String first_name, String last_name, String department) {
+	private int user_id;
+	private String user_username;
+	private String user_password;
+	private int user_type;
+	private double available_reimbursment;
+	public Users() {
 		super();
-		this.username = username;
-		this.password = password;
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.department = department;
+		// TODO Auto-generated constructor stub
 	}
-	public String getUsername() {
-		return username;
+	public Users(int user_id, String user_username, String user_password, int user_type,
+			double available_reimbursment) {
+		super();
+		this.user_id = user_id;
+		this.user_username = user_username;
+		this.user_password = user_password;
+		this.user_type = user_type;
+		this.available_reimbursment = available_reimbursment;
 	}
-	public void setUsername(String username) {
-		this.username = username;
+	public int getUser_id() {
+		return user_id;
 	}
-	public String getPassword() {
-		return password;
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+	public String getUser_username() {
+		return user_username;
 	}
-	public String getFirst_name() {
-		return first_name;
+	public void setUser_username(String user_username) {
+		this.user_username = user_username;
 	}
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
+	public String getUser_password() {
+		return user_password;
 	}
-	public String getLast_name() {
-		return last_name;
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
 	}
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
+	public int getUser_type() {
+		return user_type;
 	}
-	public String getDepartment() {
-		return department;
+	public void setUser_type(int user_type) {
+		this.user_type = user_type;
 	}
-	public void setDepartment(String department) {
-		this.department = department;
+	public double getAvailable_reimbursment() {
+		return available_reimbursment;
+	}
+	public void setAvailable_reimbursment(double available_reimbursment) {
+		this.available_reimbursment = available_reimbursment;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(available_reimbursment);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + user_id;
+		result = prime * result + ((user_password == null) ? 0 : user_password.hashCode());
+		result = prime * result + user_type;
+		result = prime * result + ((user_username == null) ? 0 : user_username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Users other = (Users) obj;
+		if (Double.doubleToLongBits(available_reimbursment) != Double.doubleToLongBits(other.available_reimbursment))
+			return false;
+		if (user_id != other.user_id)
+			return false;
+		if (user_password == null) {
+			if (other.user_password != null)
+				return false;
+		} else if (!user_password.equals(other.user_password))
+			return false;
+		if (user_type != other.user_type)
+			return false;
+		if (user_username == null) {
+			if (other.user_username != null)
+				return false;
+		} else if (!user_username.equals(other.user_username))
+			return false;
+		return true;
 	}
 	@Override
 	public String toString() {
-		return "Users [username=" + username + ", password=" + password + ", first_name=" + first_name + ", last_name="
-				+ last_name + ", department=" + department + "]";
+		return "Users [user_id=" + user_id + ", user_username=" + user_username + ", user_password=" + user_password
+				+ ", user_type=" + user_type + ", available_reimbursment=" + available_reimbursment + "]";
 	}
-	
 	
 }
