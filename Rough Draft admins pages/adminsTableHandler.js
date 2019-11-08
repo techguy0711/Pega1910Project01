b0cc4cd6bb884a165d73705f962a5ce7a4aba4d6
“Kristhian_Deoliveira” <kristhian.deoliveira@gmail.com.com>
2019-11-08 09:20:16 -0500

    Added website with most JS functionality

---
+++ Rough Draft admins pages/adminsTableHandler.js
@@ -0,0 +1,8 @@
+  document.getElementById("formToggleButton").addEventListener("click", function(){
+	  var doc = document.getElementById("TablecontentDivider");
+	  if (doc.style.display === "none") {
+		  doc.style.display = "block";
+	  } else {
+		  doc.style.display = "none";
+	  }
+  });
