22aa925fa426beb4004a4dacafdd0ac2852f732c
“Kristhian_Deoliveira” <kristhian.deoliveira@gmail.com.com>
2019-11-10 20:29:31 -0500

    github cleanup

--- Rough Draft admins pages/adminsTableHandler.js
+++
@@ -1,8 +0,0 @@
-  document.getElementById("formToggleButton").addEventListener("click", function(){
-	  var doc = document.getElementById("TablecontentDivider");
-	  if (doc.style.display === "none") {
-		  doc.style.display = "block";
-	  } else {
-		  doc.style.display = "none";
-	  }
-  });
